6b31c99de09402778afa202d1824c012deb9db6c
Dmytro <dima.shutko5@gmail.com>
2026-03-31 16:29:36 +0200

    sol2

--- task.sql
+++ task.sql
@@ -1,15 +1,15 @@
 USE ShopDB;
 
-START TRANSACTION;
-
 INSERT INTO Orders (CustomerID, Date)
-	VALUES (1, '2023-01-10');
+	VALUES(1, '2023-01-20');
+
+START TRANSACTION;
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 	VALUES  (LAST_INSERT_ID(), 1, 1);
 
 UPDATE Products
-SET WarehouseAmount = WarehouseAmount - 1
-WHERE ID = 1;
+	SET WarehouseAmount = WarehouseAmount - 1
+	WHERE ID = 1;
 
 COMMIT;
